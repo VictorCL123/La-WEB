7ee78feeda78cad2e2fd40656ab7e5e9f674cb74
VictorCL123 <cuevas-17@live.com.mx>
2019-05-01 19:56:03 -0500

    funcion menu

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,9 @@ window.addEventListener("load",menu);
 function menu(){
   document.getElementById("Menu").innerHTML = "<a href='#'>Inicio</a>"
                                               +"<a href='#'>SVG vs Canvas</a>"
+                                              +"<a href='#'>Libreria </a>"
                                               +"<a href='#'>Libreria 1</a>"
                                               +"<a href='#'>Libreria 2</a>"
-                                              +"<a href='#'>Libreria 2</a>"
-                                              +"<a href='#'>Libreria 4</a>"
-                                              +"<a href='#'>Salir</a>";
+                                              +"<a href='#'>Libreria 3</a>"
+                                              +"<a href='#'>Libreria 4</a>";
 }
